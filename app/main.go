9b8558450e6fc3fa1cc4474555f071726e0592ba
Amith Nair <amithnair@in-pune-namith.local>
2018-08-15 01:09:53 +0530

    add sample main

--- app/main.go
+++ app/main.go
@@ -1,5 +1,20 @@
 package main
 
-func main(){
+import (
+	"fmt"
+	"go-downloader/lib"
+)
+
+func main() {
+	file := lib.File{}
+	client := lib.HTTPClient{}
+	client.NewHttpClient()
+	downloader := lib.Downloader{FileUtils: &file, Client: &client}
+	println("Start Download of File")
+
+	err := downloader.DownloadFileConcurrent("./", "http://dynamodb-local.s3-website-us-west-2.amazonaws.com/dynamodb_local_2016-05-17.zip", 7)
+
+	println(fmt.Sprintf("%v", err))
+	println("Finished Downloading File")
 
 }
